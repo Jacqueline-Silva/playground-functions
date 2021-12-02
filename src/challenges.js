a9b0151bfaed323cf1a9296f4796f96bdea88097
Jacqueline Silva <jacque.sx@hotmail.com>
2021-12-02 17:12:11 -0300

    Requisitos do 1 - 4 completos

--- src/challenges.js
+++ src/challenges.js
@@ -1,22 +1,29 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(AnaPassouNoTeste, PedroPassouNoTeste) {
+  if (AnaPassouNoTeste === true && PedroPassouNoTeste === true) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base*height) / 2
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(testeDaFrase) {
+  let separandoAsPalavras = testeDaFrase.split (" ", testeDaFrase.lenght);
+  return separandoAsPalavras;
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
-}
+function concatName(arrayDeStrings) {
+  let concatenacao = arrayDeStrings[arrayDeStrings.length -1] + ", " + arrayDeStrings[0];
+  return concatenacao
+} 
+
 
 // Desafio 5
 function footballPoints() {
