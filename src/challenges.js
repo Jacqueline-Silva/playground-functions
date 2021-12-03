be1e9f82323df8334ed8aa59f4348a5c9f722c43
Jacqueline Silva <jacque.sx@hotmail.com>
2021-12-03 12:13:29 -0300

    Requisito 9

--- src/challenges.js
+++ src/challenges.js
@@ -86,12 +86,37 @@ function fizzBuzz(arrayNumerosDivisiveis) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
-}
-function decode() {
-  // seu código aqui
-}
+function encode(frase) {
+  let trocaPorNumeros = '';
+  for (let index = 0; index < frase.length; index +=1) {
+    
+      trocaPorNumeros = frase.replace(/a/g, '1')
+      .replace(/e/g, '2')
+      .replace(/i/g, '3')
+      .replace(/o/g, '4')
+      .replace(/u/g, '5')
+  } 
+return trocaPorNumeros
+} 
+
+//https://blog.betrybe.com/javascript/javascript-replace/
+
+function decode(numeros) {
+  let trocaPorVogais = '';
+  for (let index = 0; index < numeros.length; index +=1) {
+      trocaPorVogais = numeros.replace(/1/g, 'a')
+      .replace(/2/g, 'e')
+      .replace(/3/g, 'i')
+      .replace(/4/g, 'o')
+      .replace(/5/g, 'u')
+  } 
+return trocaPorVogais
+} 
+
+
+
+
+
 
 module.exports = {
   calcArea,
