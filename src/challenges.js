9c7f57ff3e38369373866b1825ffa8efd031b53e
Jacqueline Silva <jacque.sx@hotmail.com>
2021-12-03 10:39:11 -0300

    Requisitos obrigatórios 1-8

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,6 @@
 // Desafio 1
 function compareTrue(AnaPassouNoTeste, PedroPassouNoTeste) {
-  if (AnaPassouNoTeste === true && PedroPassouNoTeste === true) {
+  if (AnaPassouNoTeste && PedroPassouNoTeste === true) {
     return true;
   } else {
     return false;
@@ -30,31 +30,59 @@ function footballPoints(wins, ties) {
   let vitorias = wins * 3;
   let empates = ties * 1;
   let pontuacao = vitorias + empates;
-return pontuacao
-} 
+  return pontuacao;
+}
 
 // Desafio 6
 function highestCount(arrayDeNumeros) {
   let total = 0;
   let maior = 0;
   for (let i = 0; i < arrayDeNumeros.length; i += 1) {
-      maior =  Math.max.apply(null, arrayDeNumeros);
+    maior = Math.max.apply(null, arrayDeNumeros);
     if (maior === arrayDeNumeros[i]) {
-        total += 1;
-      }
+      total += 1;
+    }
   }
   return total;
-} 
-
+}
 
 // Desafio 7
-function catAndMouse() {
-  //
+function catAndMouse(mouse, cat1, cat2) {
+  let distanciaGatoUm = Math.abs(mouse - cat1);
+  let distanciaGatoDois = Math.abs(mouse - cat2);
+  if (distanciaGatoUm < distanciaGatoDois) {
+    return 'cat1';
+  } else if (distanciaGatoDois < distanciaGatoUm) {
+    return 'cat2';
+  } else if (distanciaGatoUm === distanciaGatoDois) {
+    return 'os gatos trombam e o rato foge';
+  }
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(arrayNumerosDivisiveis) {
+  let armazenaDivisiveis = [];
+  for (let i = 0; i < arrayNumerosDivisiveis.length; i += 1) {
+    if (
+      arrayNumerosDivisiveis[i] % 3 == 0 &&
+      arrayNumerosDivisiveis[i] % 5 !== 0
+    ) {
+      armazenaDivisiveis.push('fizz');
+    } else if (
+      arrayNumerosDivisiveis[i] % 5 == 0 &&
+      arrayNumerosDivisiveis[i] % 3 !== 0
+    ) {
+      armazenaDivisiveis.push('buzz');
+    } else if (
+      arrayNumerosDivisiveis[i] % 3 == 0 &&
+      arrayNumerosDivisiveis[i] % 5 == 0
+    ) {
+      armazenaDivisiveis.push('fizzBuzz');
+    } else {
+      armazenaDivisiveis.push('bug!');
+    }
+  }
+  return armazenaDivisiveis;
 }
 
 // Desafio 9
